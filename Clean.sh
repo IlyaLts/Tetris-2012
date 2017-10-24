057eb47c0b8122c895188d2f268c94421ca71214
Ilya Lyakhovets <ilyalyakhovets@gmail.com>
2017-10-24 08:52:38 -0700

    Remove 32-bit support.

--- Clean.sh
+++ Clean.sh
@@ -4,10 +4,3 @@ find . -name _RELEASE_32_ -exec rm -rf {} \;
 find . -name _RELEASE_64_ -exec rm -rf {} \;
 
 find -name '*.log' -delete;
-
-# MSVC Files
-find -name '*.ncb' -delete;
-find -name '*.sdf' -delete;
-find -name '*.suo' -delete;
-find -name '*.user' -delete;
-find -name '*.opensdf' -delete;
